ade7ea2546d4964f7797876849d33b62aa4f2c4d
L.W.L <102495553@qq.com>
2019-12-31 17:11:01 +0800

    feature: 歌手详情页基本完成

--- src/views/singer/singer-detail.vue
+++ src/views/singer/singer-detail.vue
@@ -1,8 +1,6 @@
 <template>
   <transition appear name="slide">
-    <div class="singer-detail">
-      {{singer.id}} : {{singer.name}}
-    </div>
+    <music-list :title="title" :bg-image="bgImage" :songs="songs"></music-list>
   </transition>
 </template>
 
@@ -11,9 +9,27 @@ import { mapGetters } from 'vuex'
 import { getSingerDetail } from '@/api/singer'
 import { ERR_OK } from '@/api/config'
 import { createSong, processSongsUrl } from '@/common/Song'
+import MusicList from '@/components/music-list/music-list'
 
 export default {
   name: 'singerDetail',
+  components: {
+    MusicList
+  },
+  data() {
+    return {
+      songs: []
+    }
+  },
+  computed: {
+    title() {
+      return this.singer.name
+    },
+    bgImage() {
+      return this.singer.avatar
+    },
+    ...mapGetters(['singer'])
+  },
   created() {
     if (!this.singer.id) {
       return this.$router.push('/singer')
@@ -39,32 +55,13 @@ export default {
       })
       return ret
     }
-  },
-  data() {
-    return {
-      songs: []
-    }
-  },
-  computed: {
-    ...mapGetters(['singer'])
   }
 }
 </script>
 
 <style lang="stylus" scoped>
-  @import "~assets/stylus/variable";
-
   .slide-enter-active, .slide-leave-active
     transition all .3s
   .slide-enter, .slide-leave-to
     transform translate3d(100%, 0, 0)
-
-  .singer-detail
-    position fixed
-    top 0
-    left 0
-    right 0
-    bottom 0
-    background $color-background
-    z-index 100
 </style>
